3714ca28391df91d5b08654c445b0d98e15288af
bruce <bruce@aeris.io>
2017-01-16 01:20:00 +0800

    input completed, godep save completed

--- logo.go
+++ logo.go
@@ -2,29 +2,53 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"unsafe"
+	termbox "github.com/nsf/termbox-go"
+	// "os"
+	// "unsafe"
 )
 
-func LoginInit() {
-	gamelogo()
-	b := make([]byte, 100)
-	f := os.Stdin
-	w := os.Stdout
-	defer f.Close()
-	defer w.Close()
+func LogoInit() {
+	//gamelogo()
+	// b := make([]byte, 100)
+	// f := os.Stdin
+	// w := os.Stdout
+	// defer f.Close()
+	// defer w.Close()
+	// for {
+	// 	w.WriteString("input:")
+	// 	c, err := f.Read(b)
+	// 	if err != nil {
+	// 		continue
+	// 	}
+	// 	bb := b[:c-1]
+	// 	b = make([]byte, 100)
+	// 	str := *(*string)(unsafe.Pointer(&bb))
+	// 	fmt.Println(str)
+	// 	if str == "exit" {
+	// 		break
+	// 	}
+	// }
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+Loop:
 	for {
-		w.WriteString("input:")
-		c, err := f.Read(b)
-		if err != nil {
-			continue
-		}
-		bb := b[:c-2]
-		b = make([]byte, 100)
-		str := *(*string)(unsafe.Pointer(&bb))
-		fmt.Println(str)
-		if str == "exit" {
-			break
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyArrowUp:
+				fmt.Println("You press 1")
+			case termbox.KeyArrowDown:
+				fmt.Println("You press F1")
+
+			default:
+				fmt.Println(ev.Key)
+				//break Loop
+				break Loop
+			}
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"HuarongPuzzle/constant"
+	//"HuarongPuzzle/constant"
 	"HuarongPuzzle/models"
-	"fmt"
+	//"fmt"
 )
 
 func init() {
@@ -13,27 +13,28 @@ func init() {
 }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		var str string
-		for j := 0; j < 8; j++ {
-			str += models.Checkerboards[models.KeyFormat([]int{i, j})].(models.Checkerboard).Name
-		}
-		fmt.Println(str)
+	// for i := 0; i < 10; i++ {
+	// 	var str string
+	// 	for j := 0; j < 8; j++ {
+	// 		str += models.Checkerboards[models.KeyFormat([]int{i, j})].(models.Checkerboard).Name
+	// 	}
+	// 	fmt.Println(str)
 
-	}
-	err := Move("B3", constant.DOWN)
-	if err != nil {
-		fmt.Println(err)
-		return
-	} else {
-		for i := 0; i < 10; i++ {
-			var str string
-			for j := 0; j < 8; j++ {
-				str += models.Checkerboards[models.KeyFormat([]int{i, j})].(models.Checkerboard).Name
-			}
-			fmt.Println(str)
-		}
-	}
+	// }
+	// err := Move("B3", constant.DOWN)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// } else {
+	// 	for i := 0; i < 10; i++ {
+	// 		var str string
+	// 		for j := 0; j < 8; j++ {
+	// 			str += models.Checkerboards[models.KeyFormat([]int{i, j})].(models.Checkerboard).Name
+	// 		}
+	// 		fmt.Println(str)
+	// 	}
+	// }
+	LogoInit()
 	// err = Move("B2", constant.DOWN)
 	// if err != nil {
 	// 	fmt.Println(err)
